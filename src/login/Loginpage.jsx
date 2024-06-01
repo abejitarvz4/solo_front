e331309decabaff9f506094fcc7ac40969402ace
Andrea <al22304031@utslrc.edu.mx>
2024-06-01 16:52:13 -0700

    agregando

--- src/login/Loginpage.jsx
+++ src/login/Loginpage.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './loginpage.css';
 
 export const Loginpage = () => {
   return (
-    <h1>Loginpage</h1>
-  )
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <p>Sign in to continue</p>
+        <form>
+          <label htmlFor="email">CORREO ELECTRÓNICO</label>
+          <input type="email" id="email" name="email" placeholder="correo electronico" />
+          
+          <label htmlFor="password">CONTRASEÑA</label>
+          <input type="password" id="password" name="password" />
+          
+          <button type="submit">log in</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Loginpage;
--- src/register/Registerpage.jsx
+++ src/register/Registerpage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './registerpage.css';
 
 export const Registerpage = () => {
   return (
-    <div>R</div>
-  )
+    <div className="register-container">
+      <div className="register-box">
+        <h2>Sign Up</h2>
+        <p>Create an account</p>
+        <form>
+          <label htmlFor="fullname">NOMBRE</label>
+          <input type="text" id="fullname" name="fullname" placeholder="tu nombre" />
+          
+          <label htmlFor="email">CORREO ELECTRÓNICO</label>
+          <input type="email" id="email" name="email" placeholder="tu correo electrónico" />
+          
+          <label htmlFor="password">CONTRASEÑA</label>
+          <input type="password" id="password" name="password" />
+          
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Registerpage;
